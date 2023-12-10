cc375fe5386baccd73974e39525da7b8e60f7ad0
Sedo-KFM <fedor122448@gmail.com>
2023-12-10 17:29:13 +0400

    Update clear and init scripts

--- clear.py
+++ clear.py
@@ -1,7 +1,16 @@
 import os
 
-for file in os.listdir('polls'):
-    os.remove(f'polls/{file}')
-os.rmdir('polls')
-os.remove('config.json')
-os.remove('subscribed.csv')
+if 'polls' in os.listdir():
+    if 'active' in os.listdir('polls'):
+        for file in os.listdir('polls/active'):
+            os.remove(f'polls/active/{file}')
+        os.rmdir('polls/active')
+    if 'archive' in os.listdir('polls'):
+        for file in os.listdir('polls/archive'):
+            os.remove(f'polls/archive/{file}')
+        os.rmdir('polls/archive')
+    os.rmdir('polls')
+if 'config.json' in os.listdir():
+    os.remove('config.json')
+if 'subscribed.csv' in os.listdir():
+    os.remove('subscribed.csv')
--- initialize.py
+++ initialize.py
@@ -2,16 +2,30 @@ import json
 import pandas as pd
 import os
 
-bot_token = input('Input bot token >>> ')
-bot_holder = int(input('Input bot holder id >>> '))
-json.dump({'bot_token': bot_token,
-           'bot_holder': bot_holder,
-           'admin_list': [bot_holder]},
-          open("config.json", "w"),
-          indent=4)
+if 'config.json' not in os.listdir():
+
+    bot_token = input('Input bot token >>> ')
+    if bot_token.strip() == '':
+        bot_token = dict(json.load(open('local_setup.json', encoding='utf-8'))).get('bot_token', '')
+
+    bot_holder = input('Input bot holder id >>> ')
+    if bot_holder == '':
+        bot_holder = dict(json.load(open('local_setup.json', encoding='utf-8'))).get('bot_holder', '')
+    bot_holder = int(bot_holder)
+
+    json.dump({'bot_token': bot_token,
+               'bot_holder': bot_holder,
+               'admin_list': [bot_holder]},
+              open("config.json", "w"),
+              indent=4)
 
 if 'polls' not in os.listdir():
     os.mkdir('polls')
+if 'active' not in os.listdir('polls'):
+    os.mkdir('polls/active')
+if 'archive' not in os.listdir('polls'):
+    os.mkdir('polls/archive')
 
-subscribed = pd.DataFrame(columns=['id'])
-subscribed.to_csv('subscribed.csv', index=False)
+if 'subscribed.csv' not in os.listdir():
+    subscribed = pd.DataFrame(columns=['id'])
+    subscribed.to_csv('subscribed.csv', index=False)
